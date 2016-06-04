d1bc83803b1b8d81b2c182e06d5cbc8cbf4e1414
cosdy <cosdy@sina.com>
2016-06-04 20:56:11 +0800

    Revert "version005"
    This reverts commit 074653696b5b1c1ec959a89e55849af7052af7d3.

--- src/android/activity/PickOrTakeImageActivity.java
+++ src/android/activity/PickOrTakeImageActivity.java
@@ -220,8 +220,8 @@ public class PickOrTakeImageActivity extends Activity implements View.OnClickLis
     protected void initData() {
         inflater = LayoutInflater.from(this);
 
-        allImages = new ArrayList<SingleImageModel>();
-        imageDirectories = new ArrayList<SingleImageDirectories>();
+        allImages = new ArrayList<>();
+        imageDirectories = new ArrayList<>();
 
         handler = new MyHandler(this);
         //默认显示全部图片
